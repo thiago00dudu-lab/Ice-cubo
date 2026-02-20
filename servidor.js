45f5ad17ac6b75861850f4982a1efa15ae03fb9d
thiago00dudu-lab <thiago00dudu@gmail.com>
2026-02-20 16:34:18 -0300

    Update servidor.js

--- servidor.js
+++ servidor.js
@@ -1,19 +1,16 @@
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config();
 
-const app = express(); // ← ESSA LINHA estava faltando
+const app = express(); // ESSA LINHA É OBRIGATÓRIA
 
 app.use(cors());
 app.use(express.json());
 
-// ROTA TESTE
 app.get("/", (req, res) => {
   res.send("Servidor funcionando 🚀");
 });
 
-// SUA ROTA
-app.post("/cadastro", async (req, res) => {
+app.post("/cadastro", (req, res) => {
   res.json({ mensagem: "Cadastro funcionando!" });
 });
 
