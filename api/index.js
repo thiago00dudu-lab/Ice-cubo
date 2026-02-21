ab105aac50cfecdf065edd5309d48a8693a7c5a9
thiago00dudu-lab <thiago00dudu@gmail.com>
2026-02-21 20:35:03 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,5 +1,3 @@
-export default function handler(req,res){
-  res.status(200).send(`<!doctype html><meta name=viewport content="width=device-width,initial-scale=1">
-  <body style="margin:0;font-family:sans-serif;background:#0f172a;color:#fff;display:flex;align-items:center;justify-content:center;height:100vh;text-align:center">
-  <div><h1>ICE CUBO 🚀</h1><p>Online na Vercel ✅</p></div></body>`)
+export default function handler(req, res) {
+  res.status(200).send("ICE CUBO Online na Vercel ✅");
 }
